076f39ca4135992d4ac9944ee7752333576bb36b
Mochaeng <campos.antoonio@gmail.com>
2023-08-02 18:18:33 -0300

    feat: configs

---
+++ farm_code/scripts/Grid.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass
+
+func _draw() -> void:
+	# vertical lines
+	for i in Utils.CELLS_AMOUNT.x:
+		var from := Vector2(i * Utils.CELL_SIZE.x, 0)
+		var to := Vector2(from.x, Utils.GRID_SIZE.y)
+		draw_line(from, to, Utils.BLACK)
+
+	# horinzontal lines
+	for i in Utils.CELLS_AMOUNT.y:
+		var from := Vector2(0, Utils.CELL_SIZE.y * i)
+		var to := Vector2(Utils.GRID_SIZE.x, from.y)
+		draw_line(from, to, Utils.BLACK)
+
+
+
---
+++ farm_code/scripts/utils.gd
@@ -0,0 +1,8 @@
+extends Node
+
+const GRID_SIZE := Vector2(800, 600)
+const CELL_SIZE := Vector2(16, 16)
+const CELLS_AMOUNT := Vector2(GRID_SIZE.x / CELL_SIZE.x, GRID_SIZE.y / CELL_SIZE.y)
+
+const BLACK := Color("23213D")
+const GRAY := Color("B9B5C3")
